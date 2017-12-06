0dda81819aaeb1ec4fead6bec4857c3c3bbe40aa
gavin <1657919948@qq.com>
2017-12-06 11:32:49 +0800

    optimise the code

--- index.js
+++ index.js
@@ -2,49 +2,34 @@ const fs = require("fs");
 const xlsx = require("node-xlsx");
 var filePath = "C:\\Users\\Gavin\\Desktop\\dataBase.xlsx";
 var obj = xlsx.parse(filePath);
-console.log(JSON.stringify(obj));
 var arr_data = obj[0].data;
+var str = "";
 for (let i = 0; i < arr_data.length; i++) {
   arr_ef = arr_data[i];
-  console.log(
+  str +=
     '{"group":"roadMobile","key":"' +
-      arr_ef[0] +
-      "-" +
-      arr_ef[1] +
-      "-" +
-      arr_ef[2] +
-      '",' +
-      '"value":{"co":' +
-      arr_ef[3] +
-      ',"hc":' +
-      arr_ef[4] +
-      ',"nox":' +
-      arr_ef[5] +
-      ',"pm2_5":' +
-      arr_ef[6] +
-      ',"pm10":' +
-      arr_ef[7] +
-      ',"sulphurContent":' +
-      arr_ef[8] +
-      "}"
-  );
-  console.log(
     arr_ef[0] +
-      "-" +
-      arr_ef[1] +
-      "-" +
-      arr_ef[2] +
-      "," +
-      arr_ef[3] +
-      "," +
-      arr_ef[4] +
-      "," +
-      arr_ef[5] +
-      "," +
-      arr_ef[6] +
-      "," +
-      arr_ef[7] +
-      "," +
-      arr_ef[8]
-  );
+    "-" +
+    arr_ef[1] +
+    "-" +
+    arr_ef[2] +
+    '",' +
+    '"value":{"co":' +
+    arr_ef[3] +
+    ',"hc":' +
+    arr_ef[4] +
+    ',"nox":' +
+    arr_ef[5] +
+    ',"pm2_5":' +
+    arr_ef[6] +
+    ',"pm10":' +
+    arr_ef[7] +
+    ',"sulphurContent":' +
+    arr_ef[8] +
+    "}},";
 }
+console.log(str);
+var filename = "C:\\Users\\Gavin\\Desktop\\dataBase.txt";
+fs.writeFile(filename, str, function(err) {
+  if (err) throw err;
+});
